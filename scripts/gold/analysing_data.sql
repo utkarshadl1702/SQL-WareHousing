8cd0df62758a4410828f5b34607f5d79f97e5e69
Utkarsh <utkarshadlakha1702@gmail.com>
2026-08-20 01:08:13 +0530

    Making gold layer (fact sales)

--- scripts/gold/analysing_data.sql
+++ scripts/gold/analysing_data.sql
@@ -134,3 +134,29 @@ LEFT JOIN silver.erp_cat_info AS pc
  ON pn.cat_id=pc.id
 WHERE prd_end_dt IS NULL 
 )t GROUP BY prd_key HAVING COUNT(*)>1
+
+
+-------------------------------Sales Fact-------------------------------
+
+-- lots of transactions and data so consider this as fact
+-- Connect with the dimensions
+
+CREATE VIEW gold.fact_sales AS
+SELECT 
+sd.sls_ord_num AS order_number,
+-- sd.sls_prd_key,
+pr.product_key,--Surrogate key
+-- sd.sls_cust_id,
+cu.customer_id, -- Surrogate key
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sd.sls_quantity AS sales_quantity,
+sd.sls_price AS price
+
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr 
+ON sd.sls_prd_key=pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id=cu.customer_id
